28c33826307737e7cfa2b83eb151f03c7470051f
Asma Chowdhury <asma.chowdhury@uasdc.org>
2017-08-01 19:33:00 +0000

    Setting up space

--- myscripts.js
+++ myscripts.js
@@ -1,3 +1,16 @@
-$(document).ready(function(){
+$('ul li').click(function(){
+    
+    var clicked_href = $(this).children('a').attr('href');
+    $('img').hide();
+    $(clicked_href).show();
 
+
+
+    var clicked_tab = $(this);
+    
+    
+    clicked_tab.addClass('active');
+    
+    
+    clicked_tab.parent().children('li').not(clicked_tab).removeClass('active');
 });
