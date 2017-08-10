ae3b109489b5718a9149c9707d3bff70fde443f9
Asma Chowdhury <asma.chowdhury@uasdc.org>
2017-08-10 19:23:37 +0000

    commit message

--- myscripts.js
+++ myscripts.js
@@ -14,3 +14,56 @@ $('ul li').click(function(){
     
     clicked_tab.parent().children('li').not(clicked_tab).removeClass('active');
 });
+
+$(document).ready(function(){
+    var bag;
+    if (localStorage.bag) {
+        bag = JSON.parse(localStorage.bag);
+    } else {
+        bag = [];
+       
+    }
+     localStorage.setItem("bag", JSON.stringify(bag));
+    
+$("#add").click(function(){
+    var item = $("#item").html();
+  var quantity = $("#quantity").val();
+  var size = $("#size").val();
+  var color = $("#color").val();
+  var price =$(".price").html();
+   $(".modal-body").append("Item: " + item);
+   $(".modal-body").append("Quantity: " + quantity);
+   $(".modal-body").append("Size: " + size);
+   $(".modal-body").append("Color: " + color);
+    bag.push({
+         item: item,
+         price: price,
+         quantity: quantity,
+         size : size,
+         color:color
+    });
+    localStorage.setItem("bag", JSON.stringify(bag));
+});
+
+if($("body#checkout").length){
+    var bag = JSON.parse(localStorage.bag);
+    if (bag.length){
+        console.log(localStorage.bag);
+        for(var count=0;count<bag.length;count=count+1){
+            console.log(bag[count]);
+            $(".cart").append(bag[count].item);
+            $(".cart").append(bag[count].price);
+            $(".cart").append("<br>");
+
+
+        }
+    }else{
+        $(".cart").append("No Items in the Cart");
+    }
+}
+$("#empty").click(function(){
+    localStorage.removeItem("bag")
+    location.reload();
+});
+});
+
